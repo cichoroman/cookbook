f51dbcb262424603a878ced058a6e58f86566a5c
cichoroman <35227357+cichoroman@users.noreply.github.com>
2019-01-12 23:05:47 +0100

    Edycja

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,21 @@ class PostsController < ApplicationController   # "<"dziedziczy z root kontroler
   def new
     @post = Post.new
   end
+
+  def edit
+
+      @post = Post.find(params[:id])
+
+  end
+  def update
+      @post = Post.find(params[:id])
+      if(@post.update(post_params))
+       redirect_to @post
+      else
+        render 'edit'
+      end
+  end
+
   def create
      #render plain: params[:post].inspect
     @post = Post.new(post_params)
