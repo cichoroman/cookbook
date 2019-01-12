a89c311d4724d5c27c823f70bfad25b3d084c2c7
cichoroman <35227357+cichoroman@users.noreply.github.com>
2019-01-12 23:55:42 +0100

    Dodanie strony myRecipes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
     @title = 'About us'
     @content = 'This is the about page'
   end
+
+  def myrecipes
+    @title = 'My recipes'
+    @content = 'Some of my favorite recipes'
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'posts#index' , as: 'home'   #nazwa kontrolera/widoku
 get 'about' => 'pages#about' , as: 'about' # standardowy rout
+get 'myrecipes' => 'pages#myrecipes' , as: 'myrecipes'
 resources :posts
 end
