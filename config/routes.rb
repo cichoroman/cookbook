9a395c81146b3854079705bc6c4d3e73d5d2f313
cichoroman <35227357+cichoroman@users.noreply.github.com>
2019-01-12 19:25:04 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'posts#index' , as: 'home'   #nazwa kontrolera/widoku
+get 'about' => 'pages#about' , as: 'about' # standardowy rout
+resources :posts
+end
